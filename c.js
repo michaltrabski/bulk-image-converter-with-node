dde653050e52f4f328e75057ed742c1b15eb7ebe
Michal Trabski <instruktormichaltrabski@gmail.com>
2020-07-30 12:58:40 +0200

    initial commit

---
+++ c.js
@@ -0,0 +1,20 @@
+const path = require("path");
+const fs = require("fs");
+const sharp = require("sharp");
+
+fs.readdir(path.resolve("inputFolder"), (err, files) => {
+  files.forEach((fileName) => {
+    convert("inputFolder", "outputFolder", fileName);
+  });
+});
+
+const convert = (from, to, fileName) => {
+  console.log(fileName);
+  sharp(`./${from}/${fileName}`)
+    // .sharpen()
+    .blur(20)
+    // .resize({ width: 500 })
+    .toFile(`./${to}/${fileName}`)
+    .then((info) => info)
+    .catch((err) => err);
+};
